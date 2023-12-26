388a4aac514965840cdd83ed12f8b63b933cc88a
Bob McAleavey <bobmcaleavey@gmail.com>
2023-12-26 15:27:39 -0500

    feat(events): scaffold events route

---
+++ src/routes/(public)/events/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { HeroSection } from '$components';
+</script>
+
+<HeroSection
+	alt="People Partying with Confetti"
+	src="https://ksedelhi.s3.amazonaws.com/heroes/pablo-heimplatz-ZODcBkEohk8-unsplash.jpg"
+	title="K∑E Upcoming Events"
+>
+	<svelte:fragment slot="button"><div class="hidden" /></svelte:fragment>
+</HeroSection>
