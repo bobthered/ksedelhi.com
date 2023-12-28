660d1022e483c94ffd27a6eeb60378461f4d9e8a
Bob McAleavey <bobmcaleavey@gmail.com>
2023-12-28 08:31:05 -0500

    feat(vercel): log vercel error

--- src/routes/(public)/donate/+page.server.ts
+++ src/routes/(public)/donate/+page.server.ts
@@ -1,7 +1,13 @@
 import { prisma } from '$lib/prisma';
 
 export const load = async () => {
-	const donates = await prisma.donate.findMany();
+	let donates: { id: string; body: string; imageSrc: string; subtitle: string; title: string }[] =
+		[];
+	try {
+		donates = await prisma.donate.findMany();
+	} catch (error) {
+		console.log(error);
+	}
 	return {
 		donates
 	};
