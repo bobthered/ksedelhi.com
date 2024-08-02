67efc102bc21fab23bf8f5e0838e2489b633f6ed
Bob McAleavey <bobmcaleavey@gmail.com>
2024-08-02 09:24:46 -0400

    feat: add PWA components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
+	import { Pwa, Title } from '$components';
 	import '../app.css';
 </script>
 
+<Pwa themeColor="#292929">
+	<link rel="alternate icon" href="/icons/icon-16x16.png" />
+	<link rel="apple-touch-icon" href="/icons/icon-apple-touch.png" />
+	<link rel="icon" type="image/svg+xml" href="/icons/icon.svg" />
+	<link rel="manifest" href="/manifest.json" />
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover"
+	/>
+	<meta name="theme-color" content="#292929" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+</Pwa>
+<Title base="KΣE at SUNY Delhi" />
 <slot />
