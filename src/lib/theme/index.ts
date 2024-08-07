0874f2f84cda7497a0a46ced7f8098fc3d54b75c
Bob McAleavey <bobmcaleavey@gmail.com>
2024-08-07 07:37:23 -0400

    feat: update theme

--- src/lib/theme/index.ts
+++ src/lib/theme/index.ts
@@ -5,10 +5,12 @@ theme.set(sveltewind);
 theme.update({
 	button:
 		'font-medium shadow-none rounded-[.25rem] dark:bg-slate-100 dark:hover:bg-slate-200 dark:focus:bg-slate-200 dark:focus:ring-slate-100/30 bg-slate-800 hover:bg-slate-700 focus:bg-slate-700 focus:ring-slate-800/30 dark:text-slate-800 dark:hover:text-slate-800 dark:focus:text-slate-800',
-	card: 'shadow-none dark:shadow-none ring-slate-950/10 dark:ring-slate-50/10',
+	card: 'shadow-none dark:shadow-none ring-slate-950/10 dark:ring-slate-50/10 rounded-[4px]',
 	header:
 		'ring-0 border-b border-slate-950/10 dark:border-slate-50/10 bg-slate-100/90 backdrop-blur dark:bg-slate-900/90',
-	nav: 'ring-0 border-b border-slate-950/10 dark:border-slate-50/10 bg-slate-100/90 backdrop-blur dark:bg-slate-900/90'
+	nav: 'ring-0 border-b border-slate-950/10 dark:border-slate-50/10 bg-slate-100/90 backdrop-blur dark:bg-slate-900/90',
+	p: 'leading-[1.7em]',
+	section: 'py-20 lg:py-32'
 });
 
 export default theme;
